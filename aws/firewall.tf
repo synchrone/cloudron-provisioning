02a9b6f5976e3ad698d19ee19be9c32f345f9b34
Aleksandr Bogdanov <syn@syn.im>
2017-08-20 10:57:46 +0200

    Enable email traffic to the instance

--- aws/firewall.tf
+++ aws/firewall.tf
@@ -21,6 +21,30 @@ resource "aws_security_group" "cloudron_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 587
+    to_port     = 587
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 993
+    to_port     = 993
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 4190
+    to_port     = 4190
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
