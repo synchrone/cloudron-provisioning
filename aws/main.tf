ce46032277e35bc54615ec4dbe23111fc3deabbf
Aleksandr Bogdanov <syn@syn.im>
2017-11-04 11:27:13 +0100

    Moving away from EBS for root volumes

--- aws/main.tf
+++ aws/main.tf
@@ -54,7 +54,7 @@ variable "backup_region" {
 }
 variable "version" {
   type = "string"
-  default = "1.6.5"
+  default = "1.7.6"
 }
 variable "cloudron_restore_url" {
   type = "string"
@@ -83,22 +83,39 @@ data "aws_caller_identity" "current" {}
 variable "images" {
   type = "map"
   default = { #from http://cloud-images.ubuntu.com/locator/ec2/
-    ap-south-1 = "ami-49e59a26"     # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    us-east-1 = "ami-d15a75c7"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    ap-northeast-1 = "ami-785c491f" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    eu-west-1 = "ami-6d48500b"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    ap-southeast-1 = "ami-2378f540" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    ca-central-1 = "ami-7ed56a1a"   # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    us-west-1 = "ami-73f7da13"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    eu-central-1 = "ami-1c45e273"   # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    sa-east-1 = "ami-34afc458"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    cn-north-1 = "ami-a163b4cc"     # 	xenial	amd64	hvm:ebs-ssd	20170303		hvm
-    us-gov-west-1 = "ami-939412f2"  # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    ap-southeast-2 = "ami-e94e5e8a" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    eu-west-2 = "ami-cc7066a8"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    ap-northeast-2 = "ami-94d20dfa" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    us-west-2 = "ami-835b4efa"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
-    us-east-2 = "ami-8b92b4ee"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+// Instance-store images here
+    ap-northeast-1 = "ami-6a89290c" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    ap-northeast-2 = "ami-b21fbadc" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    ap-south-1 = "ami-e409448b" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    ap-southeast-1 = "ami-14abeb77" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    ap-southeast-2 = "ami-9ac32cf8" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    ca-central-1 = "ami-f172ca95" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    eu-central-1 = "ami-37e25858" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    eu-west-1 = "ami-1bde7a62" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    eu-west-2 = "ami-1bbba67f" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    sa-east-1 = "ami-556a1239" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    us-east-1 = "ami-910faeeb" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    us-east-2 = "ami-9d6b44f8" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    us-west-1 = "ami-f11f2391" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+    us-west-2 = "ami-0d1fd175" # 	xenial	16.04 LTS	amd64	hvm:instance-store	20171026.1			hvm
+
+// EBS images here
+//    ap-south-1 = "ami-49e59a26"     # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    us-east-1 = "ami-d15a75c7"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    ap-northeast-1 = "ami-785c491f" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    eu-west-1 = "ami-6d48500b"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    ap-southeast-1 = "ami-2378f540" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    ca-central-1 = "ami-7ed56a1a"   # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    us-west-1 = "ami-73f7da13"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    eu-central-1 = "ami-1c45e273"   # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    sa-east-1 = "ami-34afc458"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    cn-north-1 = "ami-a163b4cc"     # 	xenial	amd64	hvm:ebs-ssd	20170303		hvm
+//    us-gov-west-1 = "ami-939412f2"  # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    ap-southeast-2 = "ami-e94e5e8a" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    eu-west-2 = "ami-cc7066a8"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    ap-northeast-2 = "ami-94d20dfa" # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    us-west-2 = "ami-835b4efa"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
+//    us-east-2 = "ami-8b92b4ee"      # 	xenial	amd64	hvm:ebs-ssd	20170619.1		hvm
   }
 }
 
